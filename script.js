54ebf3ac4f5e670fddb51586445b0bf4ee1f9695
fordexfordexsovich-a11y <fordexfordexsovich@gmail.com>
2026-06-20 09:26:45 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Поиск пользователей</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="container">
+      <h1>Поиск пользователей</h1>
+
+      <input type="text" id="search" placeholder="Введите имя" />
+
+      <div id="users"></div>
+    </div>
+
+    <script src="script.js"></script>
+  </body>
+</html>
